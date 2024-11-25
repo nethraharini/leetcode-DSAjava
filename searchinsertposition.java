9f0785718aa50c97404aba96404ca1cf63e2ff48
Nethra Harini M <nethraharini0904@gmail.com>
2024-11-25 21:44:37 +0530

    Create searchinsertposition.java

---
+++ searchinsertposition.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i =0; i<nums.length;i++){
+            if(nums[i]>=target){
+                return i;
+            }
+
+        }
+        return nums.length;
+        
+    }
+}
+
+
+/* nput
+nums =
+[1,3,5,6]
+target =
+5
+Output
+2 */
